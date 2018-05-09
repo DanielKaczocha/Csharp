f80ad16e293d30794f9fb09bf880cb46eeaaac19
Daniel <daniel.kaczocha@gmail.com>
2018-05-10 00:24:25 +0200

    Pliki C#

---
+++ MaskSharp/MaskSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskSharp
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cc = "";
+            cc = Console.ReadLine();
+
+
+            char[] Icc = new char[cc.Length];
+
+
+            for (int i = 0; i < cc.Length; i++)
+            {
+                if (i > (cc.Length - 5))
+                {
+                    Icc[i] = cc[i];
+
+                }
+                else
+                {
+                    Icc[i] = '#';
+                }
+            }
+            Console.WriteLine(Icc);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SumAllDigits/SumAllDigits/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumAllDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long digits;
+            digits = long.Parse(Console.ReadLine());
+
+            while (digits > 9)
+            {
+                int elements = 1;
+                long sum = 0;
+
+                long one_num = 0;
+                long power = 0;
+
+                elements = digits.ToString().Length - 1;
+
+                while (elements > -1)
+                {
+                    power = (long)System.Math.Pow(10, elements);
+                    one_num = digits / power;
+
+
+
+                    sum += one_num;
+                    digits -= one_num * power;
+
+                    elements--;
+
+
+                }
+                digits = sum;
+
+
+
+            }
+            Console.WriteLine((long)digits);
+            Console.ReadKey();
+        }
+    }
+}
